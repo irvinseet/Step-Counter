d4d2c9ca90208cd9309353d0035333cc4098c4d7
Irvin <uif08816@contiwan.com>
2022-07-25 11:13:26 +0800

    Initial Commit

---
+++ LowPassFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LowPassFilter
+//
+//  Created by Seet, Ting Yang Irvin (uif08816) on 25/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
